d2ead83648010ba765b541398b3522f6908a1efc
Shubham Lohra <shubhamlohra@Shubhams-MacBook-Pro.local>
2025-10-29 10:38:19 +0530

    changed time zone to ISt

---
+++ src/main/java/com/brideside/backend/config/DatabaseConfig.java
@@ -0,0 +1,48 @@
+package com.brideside.backend.config;
+
+import com.zaxxer.hikari.HikariConfig;
+import com.zaxxer.hikari.HikariDataSource;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.boot.autoconfigure.jdbc.DataSourceProperties;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Primary;
+
+import javax.sql.DataSource;
+
+@Configuration
+public class DatabaseConfig {
+
+    private static final Logger logger = LoggerFactory.getLogger(DatabaseConfig.class);
+
+    @Bean
+    @Primary
+    public DataSource dataSource(DataSourceProperties properties) {
+        logger.info("Configuring database with IST timezone...");
+        
+        HikariConfig config = new HikariConfig();
+        config.setJdbcUrl(properties.getUrl());
+        config.setUsername(properties.getUsername());
+        config.setPassword(properties.getPassword());
+        config.setDriverClassName(properties.getDriverClassName());
+        
+        // Store timestamps in UTC in database, but display in IST
+        config.setConnectionInitSql("SET time_zone = '+00:00', session.time_zone = '+00:00'");
+        
+        // Copy HikariCP settings from properties
+        config.setMaximumPoolSize(20);
+        config.setMinimumIdle(5);
+        config.setConnectionTimeout(20000);
+        config.setIdleTimeout(300000);
+        config.setMaxLifetime(1200000);
+        
+        HikariDataSource dataSource = new HikariDataSource(config);
+        
+        logger.info("Database configured with UTC timezone - timestamps stored in UTC, displayed in IST via Jackson");
+        
+        return dataSource;
+    }
+}
+
+
